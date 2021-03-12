f030631f92a36c80171df1cc806e00b33ba62908
Sanpawat Sewsuwan <koyong555@hotmail.com>
2021-03-12 13:34:31 +0700

    add scrollable view

--- src/Components/Pages/DayReport.js
+++ src/Components/Pages/DayReport.js
@@ -10,15 +10,17 @@ import '../../Styles/maskot-styling.scss';
 function DayReport() {
     return (
         <Container fluid>
-            <Row>
-                <h1 className="main-title">Main Atrium</h1>
-            </Row>
-            <Row className="main-sub-title-2">
-                <img src="/img/icons8-calendar-256.svg" className="main-item-icon"></img>
-                <h5>February 3, 2021</h5>
-                <img src="/img/icons8-calendar-256.svg" className="main-item-icon"></img>
-                <h5>10:00 HRS.</h5>
-            </Row>
+            <div className="header-view-position-fixed">
+                <Row>
+                    <h1 className="main-title">Main Atrium</h1>
+                </Row>
+                <Row className="main-sub-title-2">
+                    <img src="/img/icons8-calendar-256.svg" className="main-item-icon"></img>
+                    <h5>February 3, 2021</h5>
+                    <img src="/img/icons8-calendar-256.svg" className="main-item-icon"></img>
+                    <h5>10:00 HRS.</h5>
+                </Row>
+            </div>
             <div className="overview-box">
                 <h5 className="overview-header">Overview</h5>
                 <Row className="overview-margin-mini-card">
@@ -33,6 +35,28 @@ function DayReport() {
                     </Col>
                 </Row>
             </div>
+            <Row className="detection-result-container">
+                <Col md="4" className="temp">
+                    <DetectionResultCard></DetectionResultCard>
+                </Col>
+                <Col md="4" className="temp">
+                    <DetectionResultCard></DetectionResultCard>
+                </Col>
+                <Col md="4" className="temp">
+                    <DetectionResultCard></DetectionResultCard>
+                </Col>
+            </Row>
+            <Row className="detection-result-container">
+                <Col md="4" className="temp">
+                    <DetectionResultCard></DetectionResultCard>
+                </Col>
+                <Col md="4" className="temp">
+                    <DetectionResultCard></DetectionResultCard>
+                </Col>
+                <Col md="4" className="temp">
+                    <DetectionResultCard></DetectionResultCard>
+                </Col>
+            </Row>
             <Row className="detection-result-container">
                 <Col md="4" className="temp">
                     <DetectionResultCard></DetectionResultCard>
