7ed6246cf848898782c6cb8c8099225c67e63eec
Sanpawat Sewsuwan <koyong555@hotmail.com>
2021-04-27 15:23:11 +0700

    long time no see commit

--- src/Components/Utilities/DetectionResultCard.jsx
+++ src/Components/Utilities/DetectionResultCard.jsx
@@ -7,10 +7,21 @@ function DetectionResultCard(props) {
     return (
         <div className="detection-result-card">
             <div className="detail">
-                <h4>11:00 HRS.</h4>
+                <Row className="detail-padding">
+                    <Col md="8" className="text-container">
+                        <h4>11:00 HRS.</h4>
+                    </Col>
+                    <Col md="2">
+                        <img className="button-icon" src="/img/icons8-expand-96.png"></img>
+                    </Col>
+                    <Col md="2">
+                        <img className="button-icon" src="/img/icons8-delete-bin-90.png"></img>
+                    </Col>
+                </Row>
             </div>
             <div className="gradient-layer"></div>
             <div className="overlay-image-frame"></div>
+            <img className="profile-icon-button" src="/img/icons8-user-90.png"></img>
             <img className="overlay-image" src="/img/sample.png"></img>
             <div className="overlay"></div>
             <img className="image" src="/img/sample.png"></img>
